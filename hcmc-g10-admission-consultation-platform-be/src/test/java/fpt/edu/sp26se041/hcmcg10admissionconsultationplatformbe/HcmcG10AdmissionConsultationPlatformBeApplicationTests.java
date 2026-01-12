71793299aec7ca80563e484e5bd0485fa3b735f9
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2026-01-12 10:22:31 +0700

    feat: config file pom

---
+++ hcmc-g10-admission-consultation-platform-be/src/test/java/fpt/edu/sp26se041/hcmcg10admissionconsultationplatformbe/HcmcG10AdmissionConsultationPlatformBeApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.edu.sp26se041.hcmcg10admissionconsultationplatformbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmcG10AdmissionConsultationPlatformBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
