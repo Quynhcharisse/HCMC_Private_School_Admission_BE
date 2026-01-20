a621910a768de4576821213391d0473a5d2cdd86
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2026-01-20 12:47:46 +0700

    feat: add new dependencies

---
+++ hcmc-private-school-admission/src/main/java/fpt/edu/edubridgehcm/HcmcPrivateSchoolAdmissionApplication.java
@@ -0,0 +1,13 @@
+package fpt.edu.edubridgehcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcmcPrivateSchoolAdmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcmcPrivateSchoolAdmissionApplication.class, args);
+    }
+
+}
---
+++ hcmc-private-school-admission/src/test/java/fpt/edu/edubridgehcm/HcmcPrivateSchoolAdmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.edu.edubridgehcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmcPrivateSchoolAdmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
